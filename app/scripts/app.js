cbf8c102e4c78ee66891c53953cc49295867705d
Tony Lefler <tonylefler@gmail.com>
2014-08-10 20:57:18 -0600

    added fonts

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,22 +21,15 @@ var genomiconsApp = angular
 
 genomiconsApp
   .config(function ($stateProvider, $urlRouterProvider) {
-    asdfasdfasdf
-    $urlRouterProvider.otherwise('/icon/all');
+    $urlRouterProvider.otherwise('/all');
     
     $stateProvider
-      .state('icon', {
-        url: '/icon',
-        abstract: true,
+      .state('all', {
+        url: '/all',
         views: {
           'nav': {
             templateUrl: 'views/nav.html'
-          }
-        }
-      })
-      .state('icon.all', {
-        url: '/all',
-        views: {
+          },
           'content': {
             templateUrl: 'views/all.html'
           }
